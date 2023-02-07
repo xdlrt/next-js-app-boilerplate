1095b4852e15658a6eba1b30eff57a1fd81d3f43
yeshu <673643706@qq.com>
2023-02-07 21:38:20 +0800

    chore: update package version

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -24,16 +24,16 @@ const Main = (props: IMainProps) => (
           <ul className="flex flex-wrap text-xl">
             <li className="mr-6">
               <Link href="/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
+                <span className="border-none text-gray-700 hover:text-gray-900">
                   Home
-                </a>
+                </span>
               </Link>
             </li>
             <li className="mr-6">
               <Link href="/about/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
+                <span className="border-none text-gray-700 hover:text-gray-900">
                   About
-                </a>
+                </span>
               </Link>
             </li>
             <li className="mr-6">
